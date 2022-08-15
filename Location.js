bac159ceb993e1698ef5913ceca893c0dfda1415
Dmitrii Gubko <dalnoboy29region@gmail.com>
2022-08-15 12:31:56 -0600

    Add locations

---
+++ Location.js
@@ -0,0 +1,22 @@
+class Location {
+  constructor(name, type, enemy) {
+    this.name = name;
+    this.type = type || "forest";
+    this.enemy = enemy;
+    this.isVisited = false;
+  }
+
+  announceLocation() {
+    var locationBlock = document.getElementById("location-msg"); //get HTML element with id atribute location-msg as a js object.
+    locationBlock.innerText = `You are at the ${this.name}`; //changes inner text in HTML element, this.name refers to object property name.
+  }
+  announceEnemy() {
+    var enemyBlock = document.getElementById("enemy-msg"); //get HTML element with id atribute enemy-msg as a js object.
+    enemyBlock.innerText = `${this.enemy.name} is attacking you`; //changes inner text in HTML element, this.enemy refers to object property enemy. this.enemy is an object passed to contructor function from outside.
+  }
+  leaveLocation() {
+    this.isVisited = true;
+  }
+}
+// Exporting class Location from this file and it can be used in others.
+module.exports = Location;
--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 // imports
 
+var Location = require("./Location");
+
 // This line of code will declare a variable and assign it to a value stored from previous page
 var characterName = localStorage.getItem("character");
 
 // CODE BELOW
+var forestLocation = new Location("South Forest", "forest", { type: "ghoul" });
+var streetLocation = new Location("Concord", "street", { type: "death claw" });
+var factoryLocation = new Location("Wilson Atomatoys factory", "factory", {
+  type: "assaultron",
+});
+var badlandLocation = new Location("Dunes", "badland", { type: "raider" });
